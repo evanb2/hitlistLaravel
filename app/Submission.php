b412a7ddac043bcb6c011d524fdb2f16910447bf
Evan Butler <evanb2@mac.com>
2015-10-22 08:26:36 -0700

    Add fillable fields to Submission model

--- app/Submission.php
+++ app/Submission.php
@@ -7,6 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Submission extends Model
 {
     protected $fillable = [
-        //user fillable fields
+        'content_type',
+        'label_name',
+        'content_provider',
+        'upc_ean',
+        'artist_name',
+        'content_title',
+        'media_type',
+        'genre',
+        'format',
+        'storedate',
+        'release_type',
+        'new_artist',
+        'instant_grat',
+        'group_name',
+        'national_forecast',
+        'marketshare',
+        'itunes_forecast',
+        'release_sales',
+        'sales_history',
+        'development_cost',
+        'description',
+        'extras',
+        'comments',
+        'marketing_drivers'
     ];
 }
