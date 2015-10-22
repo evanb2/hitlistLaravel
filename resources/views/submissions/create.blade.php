e3ad4d73186cb17da062e2883d7242cfcc4fb512
Evan Butler <evanb2@mac.com>
2015-10-22 09:25:54 -0700

    Add hitlist master page, _form and errors views

---
+++ resources/views/submissions/create.blade.php
@@ -0,0 +1,10 @@
+@extends('hitlist')
+
+@section('content')
+
+    {!! Form::open(['url' => 'submissions']) !!}
+        @include('submissions._form', ['submitBtnText' => 'Submit'])
+    {!! Form::close() !!}
+
+    @include('errors.list')
+@stop
