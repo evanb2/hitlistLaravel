8a88e5a99bdcf73228f276de34dc709a396dc27e
Evan Butler <evanb2@mac.com>
2015-10-24 19:18:49 -0700

    Fix content-type alignment

--- resources/views/submissions/_form.blade.php
+++ resources/views/submissions/_form.blade.php
@@ -9,12 +9,27 @@
                     {!! Form::radio('content_type', 'VD', ['value' => 'VD']) !!}VD
                 </label>
             </div>
+        </div>
 
-            <div class="form-group" id="div_label_name">
-                {!! Form::label('label_name', 'Label Name: ', ['class' => 'col-sm-3 control-label']) !!}
-                <div class="col-sm-9">
-                    {!! Form::text('label_name', null, ['class' => 'form-control', 'id' => 'label_name']) !!}
-                </div>
+        <div class="form-group" id="div_label_name">
+            {!! Form::label('label_name', 'Label Name: ', ['class' => 'col-sm-3 control-label']) !!}
+            <div class="col-sm-9">
+                {!! Form::text('label_name', null, ['class' => 'form-control', 'id' => 'label_name']) !!}
             </div>
+        </div>
 
+        <div class="form-group" id="div_upc_ean">
+            {!! Form::label('upc_ean', 'UPC/EAN: ', ['class' => 'col-sm-3 control-label']) !!}
+            <div class="col-sm-9">
+                {!! Form::text('upc_ean', null, ['class' => 'form-control', 'id' => 'upc_ean', 'placeholder' => '123456789012']) !!}
+                <div class="alert alert-success" role="alert" id="valid">That should work!</div>
+                <div class="alert alert-danger" role="alert" id="invalid">The UPC/EAN must contain only numbers and be 12 or 13 integers in length.</div>
+            </div>
+        </div>
+
+        <div class="form-group" id="div_artist_name">
+            {!! Form::label('artist_name', 'Artist Name: ', ['class' => 'col-sm-3 control-label']) !!}
+            <div class="col-sm-9">
+                <input class="form-control" type="text" name="artist_name" id="artist_name" size="50" required>
+            </div>
         </div>
